1a6b5b38e7a5b6b53219490e4f2bc53a06e9ec23
Nicola Aitken <naitken22@gmail.com>
2014-10-01 15:22:10 +0100

    server takes post requests

--- controllers/index_controller.rb
+++ controllers/index_controller.rb
@@ -6,18 +6,34 @@ class IndexController  < HTTPServlet::AbstractServlet
     
   def index
 	  @name = "Nicola"
+	  @blogpost ||= "No blog posts"
   end
 
   def get_binding
      binding 
   end
 
-  def do_GET(request, response)
-    self.index 
+  def erb_conversion(request, response)
+  	self.index 
     template_file = File.read('./views/index.html')
     template = ERB.new(template_file)
     response.body = template.result(self.get_binding)
     response['Content-Type'] = 'text/html'
+  end
+
+  def do_GET(request, response)
+  	self.erb_conversion(request, response)
+  end
+
+  def do_POST(request, response)
+  	self.erb_conversion(request, response)
+  	@blogpost = request.body
+  	p @blogpost
+  	# @author = params[:author]
+  	# @content = params[:content]
+  	# p @author
+  	# p @content
+
   end
 
 end
